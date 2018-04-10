66da5449db2d911e7cd524acf17579ab64acac1b
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-04-10 20:14:22 +0100

    Set destintion regions in build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ PACKER=$(which packer)
 
 if [ -z ${ANSIBLE_VAULT_PASSWORD+x} ]; then echo "ANSIBLE_VAULT_PASSWORD not set"; else echo "${ANSIBLE_VAULT_PASSWORD}" > .vault_pass.txt; fi
 
-${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" build.json
+${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" -var "destination_regions=${DESTINATION_REGIONS}" build.json
