9aab4b0a37ab7e74a1ba61a4375d8602403e67e4
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-04-10 17:08:46 +0100

    Replace the building of AMIs in each region with destination regions configuration which replicates AMIs to the given regions

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ PACKER=$(which packer)
 
 if [ -z ${ANSIBLE_VAULT_PASSWORD+x} ]; then echo "ANSIBLE_VAULT_PASSWORD not set"; else echo "${ANSIBLE_VAULT_PASSWORD}" > .vault_pass.txt; fi
 
-${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" ubuntu.json
+${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" build.json
